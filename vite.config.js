f80dc90bc19aae42c4a3e6afc2eb06549cb9297e
Y-Kitaro <yhatakt101@gmail.com>
2026-04-06 22:39:08 +0900

    [update] デプロイ設定を追加

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ビルド設定
-  build: {
-    // gitHubPagesで表紙するために出力先を 'docs' に変更
-    outDir: 'docs',
-  },
   // ベースパスをリポジトリ名に変更
-  base: "/PatentViewer/", // GitHubのリポジトリ名と揃る
+  base: "/PatentViewer/", // GitHubのリポジトリ名と揃える
 })
